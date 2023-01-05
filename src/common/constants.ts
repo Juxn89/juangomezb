55d58117d5d3f13225388ce9ed59f749933565ce
Juan Gómez <gomezj01@stepstone.com>
2023-01-05 08:45:55 -0600

    Uso de constantes para las URL de las imagenes

---
+++ src/common/constants.ts
@@ -0,0 +1,4 @@
+export const FLAGS_URL = {
+	EN_FLAG: '/assets/svg/flags/gb.svg',
+	ES_FLAG: '/assets/svg/flags/es.svg'
+}
--- src/components/ui/Flags.tsx
+++ src/components/ui/Flags.tsx
@@ -1,19 +1,19 @@
 import { FC } from 'react'
 import NextLink from 'next/link'
-import Image from 'next/image'
 import { Avatar, Box, Link } from '@mui/material'
+import { FLAGS_URL } from '@common/constants'
 
 export const Flags: FC = () => {
   return (
     <Box display='flex'>
         <NextLink href='/es/' passHref legacyBehavior>
             <Link>
-                <Avatar src='/assets/svg/flags/es.svg' alt='Español' sx={{ width: 30, height: 30 }} style={{ marginRight: '5px' }} title='Español'/>
+                <Avatar src={ FLAGS_URL.ES_FLAG } alt='Español' sx={{ width: 30, height: 30 }} style={{ marginRight: '5px' }} title='Español'/>
             </Link>
         </NextLink>
-        <NextLink href='/es/' passHref legacyBehavior>
+        <NextLink href='/en/' passHref legacyBehavior>
             <Link>
-                <Avatar src='/assets/svg/flags/gb.svg' alt='English' sx={{ width: 30, height: 30 }} title='English' />
+                <Avatar src={ FLAGS_URL.EN_FLAG } alt='English' sx={{ width: 30, height: 30 }} title='English' />
             </Link>
         </NextLink>
     </Box>
