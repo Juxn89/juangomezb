bee3485a2667e5fe6c9fbd2575068cff56b6781b
Juan Gómez <gomezj01@stepstone.com>
2023-01-17 14:53:05 -0600

    Se agrega pagina para error 500

---
+++ src/pages/500.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next'
+import { MainLayout } from '@components/layouts'
+import { Typography } from '@mui/material'
+
+const InternalServerErrorPage: NextPage = () => {
+  return (
+    <MainLayout>
+        <Typography variant='h3' component='h1'>500</Typography>
+        <Typography variant='h5' component='h2'>Server-side error occurred</Typography>
+    </MainLayout>
+  )
+}
+
+export default InternalServerErrorPage
