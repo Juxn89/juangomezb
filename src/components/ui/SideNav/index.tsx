fdfbc1a9308f5f157dfeef9d02bf916604cc3a16
Juan Gómez <gomezj01@stepstone.com>
2024-06-05 10:35:04 -0600

    Enhanced side menu

--- src/components/ui/SideNav/index.tsx
+++ src/components/ui/SideNav/index.tsx
@@ -8,30 +8,30 @@ import './styles.css'
 
 export const SideNav = () => {
 	const { isOpenSideNav, closeSideNav } = useContext(PortfolioContext) as ContextType
+	const menuLinks = [
+		{ label: 'Home', link: '#home' },
+		{ label: 'Experience', link: '#WorkExperience' },
+		{ label: 'Projects', link: '#Projects' },
+		{ label: 'Skill', link: '#Skill' },
+	]
 
 	return(
-		<div className={ `${ isOpenSideNav ? 'block' : 'hidden'} SideNav absolute bg-blue-800 text-white flex flex-col`}>
+		<div className={ `${ isOpenSideNav ? 'block' : 'hidden'} SideNav absolute bg-slate-700 text-white flex flex-col`}>
 			<div className='flex flex-row-reverse p-2'>
 				<CloseOutlined className='text-2xl' onClick={ () => closeSideNav() } />
 			</div>
 			<div className='w-full'>
 				<ul>
-					<li 
-						className='text-2xl hover:bg-blue-300 hover:text-blue-950 cursor-pointer m-0 p-2 font-medium'
-					>
-						<a href='#home' onClick={ () => closeSideNav() }>Home</a>
-					</li>
-					<li 
-						className='text-2xl hover:bg-blue-300 hover:text-blue-950 cursor-pointer m-0 p-2 font-medium'
-					>
-						<a href='#WorkExperience onClick={ () => closeSideNav() }'>Experience</a>						
-					</li>
-					<li 
-						className='text-2xl hover:bg-blue-300 hover:text-blue-950 cursor-pointer m-0 p-2 font-medium'
-					>
-						<a href='#Projects' onClick={ () => closeSideNav() }>Projects</a>						
-					</li>
-					<li className='text-2xl hover:bg-blue-300 hover:text-blue-950 cursor-pointer m-0 p-2 font-medium'>Skill</li>
+					{
+						menuLinks.map(menu => (
+							<li
+								key={ menu.label }
+								className='text-2xl hover:bg-slate-500 hover:text-slate-950 cursor-pointer m-0 p-2 font-medium'
+							>
+								<a href={ menu.link } onClick={ () => closeSideNav() }> { menu.label } </a>
+							</li>
+						))
+					}
 				</ul>
 			</div>
 		</div>
